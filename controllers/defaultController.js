0ce62184a3bb9215d95c3d49aeca1a5f2d197747
Ganeshk750 <ganesh9ot@gmail.com>
2019-10-01 00:49:16 +0530

    Display Post and Catergory Module imp

--- controllers/defaultController.js
+++ controllers/defaultController.js
@@ -1,6 +1,10 @@
+const Post = require('../models/PostModel');
+const Category = require('../models/CategoryModel');
 module.exports = {
-    index: (req, res) =>{
-        res.render('default/index');
+    index: async (req, res) =>{
+        const posts = await Post.find();
+        const categories = await Category.find();
+        res.render('default/index', {posts: posts, categories: categories});
     },
     loginGet: (req, res) =>{
         res.render('default/login');
